ad2845ac7ab2b3fd70ba2b22a817473203ecd9f1
Conan Kunhwan Ahn <kunhwon@gmail.com>
2022-12-29 16:57:50 +0900

    소수 검사, 최대공약수, 최소공배수 로직 추가

---
+++ src/lib/index.js
@@ -0,0 +1,17 @@
+export * from './공배수공약수.js';
+
+/**
+ * 소수 여부 검사 로직
+ *
+ * @param {number} num
+ */
+export function isPrimeNumber(num) {
+  // 제곱근 이상의 숫자에서는 나눠지는 수가 나올 수 없음.
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
---
+++ src/lib/공배수공약수.js
@@ -0,0 +1,47 @@
+/**
+ *
+ * @param {number} num
+ */
+function check자연수(num) {
+  if (num < 0) {
+    throw new Error('입력 값이 자연수가 아닙니다');
+  }
+}
+
+/**
+ * 최대공약수를 가져오는 함수 - 유클리드 호제법
+ * 2개의 자연수 a, b(a > b)에 대해서 a를 b로 나눈 나머지가 r일 때, a와 b의 최대공약수는 b와 r의 최대공약수와 같다.
+ *
+ * @param {number} num1
+ * @param {number} num2
+ */
+export function getGcd(num1, num2) {
+  check자연수(num1);
+  check자연수(num2);
+
+  const big = num1 > num2 ? num1 : num2;
+  const small = num1 > num2 ? num2 : num1;
+
+  if (small === 0) {
+    return big;
+  }
+
+  return getGcd(small, big % small);
+}
+
+/**
+ * 최소공배수를 가져오는 함수 - 유클리드 호제법
+ * 두 수 a와 b의 최소공배수는 a와 b의 곱을 a와 b의 최대공약수를 나눈 것과 같다.
+ *
+ * @param {number} num1
+ * @param {number} num2
+ */
+export function getLcm(num1, num2) {
+  check자연수(num1);
+  check자연수(num2);
+
+  const big = num1 > num2 ? num1 : num2;
+  const small = num1 > num2 ? num2 : num1;
+
+  return (big * small) / getGcd(big, small);
+}
